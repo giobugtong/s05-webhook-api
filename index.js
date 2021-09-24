3640cf21654ba83e5d2b65a1e912ec705ccdb3d7
Gio Bugtong <nlbugtong@gmail.com>
2021-09-24 21:31:48 +0800

    Change method to POST

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const result = {};
 
 app.use(cors());
 app.get('/', (req, res) => res.send('Welcome to Webhook API!'));
-app.get("/result", (req, res) => res.send(result));
+app.post("/result", (req, res) => res.send(result));
 app.listen(port, () => console.log(`Webhook API is listening on ${port}!`));
